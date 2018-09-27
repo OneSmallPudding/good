fbfcd27b1b5cc09c53e0c884168e5483a51e17ad
python <978862522@qq.com>
2018-09-27 21:46:33 +0800

    显示右上角的状态

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,14 +1,23 @@
-from flask import current_app, render_template
+from flask import current_app, render_template, session
 from info import sr
 import logging
 
 # 2蓝图注册路由
+from info.models import User
 from info.modules.home import blu_home
 
 
 @blu_home.route('/')
 def index():
-    return render_template('index.html')
+    user_id = session.get('user_id')
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except BaseException as e:
+            current_app.logger.error(e)
+    user = user.to_dict() if user else None
+    return render_template('index.html', user=user)
     # return '1111'
 
 

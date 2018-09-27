724d8f1de842138579ce1ca855fa4afbc26f29f3
python <978862522@qq.com>
2018-09-27 22:55:38 +0800

    主页的新闻列表

--- info/modules/home/views.py
+++ info/modules/home/views.py
@@ -1,11 +1,12 @@
-from flask import current_app, render_template, session
+from flask import current_app, render_template, session, request, jsonify
 from info import sr
 import logging
 
 # 2蓝图注册路由
-from info.constants import CLICK_RANK_MAX_NEWS
+from info.constants import CLICK_RANK_MAX_NEWS, HOME_PAGE_MAX_NEWS
 from info.models import User, Category, News
 from info.modules.home import blu_home
+from info.utils.response_code import RET, error_map
 
 
 @blu_home.route('/')
@@ -35,3 +36,38 @@ def favicon():
     # flask 内置了语法，可以返回静态文件
     # flask 中内置的访问静态页面文件的路由也会调用这个方法
     return current_app.send_static_file("news/favicon.ico")
+
+
+# 获取新闻列表
+@blu_home.route('/get_news_list')
+def get_news_list():
+    # 获取请求参数
+    cid = request.args.get('cid')
+    cur_page = request.args.get('cur_page')
+    per_count = request.args.get('per_count', HOME_PAGE_MAX_NEWS)
+    # 验证请求参数
+    if not all([cid, current_app]):
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+    # 格式转换
+    try:
+        cid = int(cid)
+        cur_page = int(cur_page)
+        per_count = int(per_count)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg=error_map[RET.PARAMERR])
+    # 根据请求参数查询数据
+    filter_list = []
+    if cid != 1:
+        filter_list.append(News.category_id == cid)
+    try:
+        pn = News.query.filter(*filter_list).order_by(News.create_time.desc()).paginate(cur_page, per_count)
+    except BaseException as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg=error_map[RET.DBERR])
+    data = {
+        "news_list": [news.to_basic_dict() for news in pn.items],
+        "total_page": pn.pages
+    }
+    # 返回数据
+    return jsonify(errno=RET.OK, errmsg=error_map[RET.OK], data=data)
